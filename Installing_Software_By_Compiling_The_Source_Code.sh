973668294c4b73b520ec02c4e9a79ef380b78650
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-30 15:30:06 +0400

    Added a shell script to explore how to work with DNF and Yum package managers

--- Installing_Software_By_Compiling_The_Source_Code.sh
+++ Installing_Software_By_Compiling_The_Source_Code.sh
@@ -11,6 +11,15 @@ git clone https://github.com/htop-dev/htop.git
 # Third is to install the libraries needed by HTOP, the exact details can be found from the README.md file
 sudo apt install libncursesw5-dev autotools-dev autoconf automake build-essential
 
-# Fourth step from the README.md file is to run the below
+# Fourth step from the README.md file is to run the below to generate a configuration script
+cd htop;
 ./autogen
 
+# Fifth is to run configure 
+./configure
+
+# Sixth step is to run make as below
+make 
+
+# Sevent step is to make the htop application accessible without typing the full path when invoking it
+sudo make install
---
+++ Update_Software_Centos.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# To check what software need's updating
+sudo dnf check-upgrade
+
+# To upgrade all the latest software
+sudo dnf upgrade -y
+
+# Managing software in Centos or Redhat to get the defaults
+sudo dnf repolist
+
+# To get detailed information we can use the above command with the -v option
+sudo dnf repolist -v
+
+# To get a detailed list of repositories that we can enable we can use
+sudo dnf repolist --all
+
+# To enable a repository we can use
+sudo dnf config-manager --enable codeready-builder-for-rhel-9-rhui-debug-rpms
+sudo dnf config-manager --enable codeready-builder-for-rhel-9-rhui-rpms
+sudo dnf onfig-manager  --enable codeready-builder-for-rhel-9-rhui-source-rpms
+
+# To disable it we can use
+sudo dnf config-manager --disable codeready-builder-for-rhel-9-rhui-debug-rpms
+sudo dnf config-manager --disable codeready-builder-for-rhel-9-rhui-rpms
+sudo dnf onfig-manager  --disable codeready-builder-for-rhel-9-rhui-source-rpms
+
+# To add a repository from a third party provider like docker we can use the following command
+sudo dnf install gnome-terminal -y
+sudo dnf -y install dnf-plugins-core -y 
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Get our OS specific version
+sudo yum install docker-ce
+
+# Linux post-installation steps for Docker Engine
+sudo usermod -aG docker $USER
+newgrp docker
+
+#If prompted to accept the GPG key, verify that the fingerprint matches
+#Start Docker.
+sudo systemctl start docker
+
+# Verify that the Docker Engine installation is successful by running the hello-world image.
+docker run hello-world
+
+# If we accidentally removed a package and need to reinstall it we can use
+sudo dnf reinstall nginx
+
+# To unistall a package we can use the
+sudo dnf remove nginx
+
+# To install packages easier there is a group install option, first we need to list the groups
+sudo dnf group list
+
+# Then we can install from below group as follows
+#Available Environment Groups:
+#   Server with GUI
+#   Server
+#   Minimal Install
+#   Workstation
+#   Virtualization Host
+#   Custom Operating System
+#Available Groups:
+#   Console Internet Tools
+#   Container Management
+#   RPM Development Tools
+#   .NET Development
+#   System Tools
+#   Headless Management
+#   Smart Card Support
+#   Legacy UNIX Compatibility
+#   Security Tools
+#   Network Servers
+#   Scientific Support
+#   Graphical Administration Tools
+#   Development Tools
+sudo dnf group install "Development Tools" -y
+
