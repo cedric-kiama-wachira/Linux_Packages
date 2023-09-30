b4d1da88e48359d089b6a71a3a374aaace8cf878
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-30 12:50:49 +0400

    Modified the script to remove and autoremove the package nginx with its dependencies

--- Package_Management.sh
+++ Package_Management.sh
@@ -9,3 +9,13 @@ sudo apt update -y && sudo apt install nginx -y
 
 # To list the files included in it we use below low level tool
 sudo dpkg --listfiles nginx
+
+# To list more on  one of the following NEW packages installed:
+#  libnginx-mod-http-image-filter libnginx-mod-http-xslt-filter libnginx-mod-mail libnginx-mod-stream nginx nginx-common nginx-core
+sudo dpkg --listfiles nginx-core
+
+# If curious and we need to get more information of the additional packages installes as part of the nginx installation we can use
+sudo apt show libnginx-mod-stream
+
+# To get all package details that have the name nginx excluding description
+sudo apt search --names-only nginx
